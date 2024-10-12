12a7f559ccf63ad7b09315b3137cf6fd8c61494f
Ouassima Dihaj <ouassimadihaj1@gmail.com>
2024-10-12 22:40:26 +0100

    first commit testing01

---
+++ src/main/java/com/ouassima/springjenkins/SpringJenkinsApplication.java
@@ -0,0 +1,22 @@
+package com.ouassima.springjenkins;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+	public static Logger logger=  LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	@PostConstruct
+	public void init(String name){
+		logger.info("Application started .....");
+	}
+	public static void main(String[] args) {
+		logger.info("Application executed .....");
+
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
